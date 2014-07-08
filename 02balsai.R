c1f1bd1042f62a5cd39c8b2a1b733eb0ae98ca81
Vaidotas Zemlys <zemlys@gmail.com>
2014-07-08 14:42:24 +0300

    Add circle graphs

--- 02balsai.R
+++ 02balsai.R
@@ -28,6 +28,8 @@ mtk %>% group_by(Vieta) %>% summarise(Viso=sum(Balsai)/6) %>% arrange(-Viso)
 
 ##2. Kas laimejo
 fwin <- mtk %>% group_by(Kandidatas) %>% summarise(Viso1=sum(Balsai)) %>% arrange(-Viso1) %>% use_series(Kandidatas) %>% extract(1:6) %>% as.character
+fwin_pdal<- dtk %>% filter(Kandidatas %in% fwin) %>% use_series("Padalinys") %>% as.character
+
 
 ##3. Laimėjusių balsų pasiskirstymai
 qplot(x=Vieta,y=Balsai,data=filter(mtk,Kandidatas %in% fwin),geom="bar",stat="identity") + facet_wrap(~Kandidatas)
@@ -38,3 +40,35 @@ qplot(x=Vieta,y=Proc,data=filter(mtk,Kandidatas %in% fwin),geom="bar",stat="iden
 
 ##5. Kvorumas
 fvis <- read.csv("data/Fiz_balsavo_viso.csv")
+mtk <-  merge(mtk,fvis) %>% mutate(Proc1=Balsai/Balsavo*100)
+qplot(x=Vieta,y=Proc1,data=filter(mtk,Kandidatas %in% fwin),geom="bar",stat="identity") + facet_wrap(~Kandidatas)
+
+##6. Kas surinko virš 50% padalinyje
+mtk %>% filter(Proc1>=50) %>% group_by(Kandidatas) %>% summarize(Daugumos=n()) %>% arrange(-Daugumos)
+
+##7. Kuris padalinys balsavo už kurį:
+finst <- mtk %>% group_by(Padalinys,Vieta) %>% summarize(Viso=sum(Balsai)) 
+finst2 <- mtk %>% filter(Kandidatas %in% fwin)%>% group_by(Padalinys,Vieta) %>% summarize(Viso=sum(Balsai)) 
+finst3 <- mtk %>% filter(!(Kandidatas %in% fwin))%>% group_by(Padalinys,Vieta) %>% summarize(Viso=sum(Balsai)) 
+finst <- finst3
+
+##8. Siuntė (ty atidavė) balsų ir gavo balsų
+fsent <- finst %>% group_by(Vieta) %>% summarize(Sent=sum(Viso)) %>% arrange(-Sent) %>% set_names(c("Padalinys","Sent"))
+frec <- finst %>% group_by(Padalinys) %>% summarize(Received=sum(Viso)) %>% arrange(-Received)
+
+fsr <- merge(fsent,frec,all=TRUE)
+fsr$Sent[is.na(fsr$Sent)] <- 0
+fsr$Received[is.na(fsr$Received)] <- 0
+fsr <- fsr %>% mutate(Padalinys=as.character(Padalinys)) %>% arrange(Padalinys)
+
+fsr <- fsr %>% mutate(All=Sent+Received,col=brewer.pal(10,"Paired"))
+
+##9.
+library(circlize)
+do.circle(finst)
+
+
+aa <- finst %>% filter(Vieta=="MF")
+aa$end <- cumsum(aa$Viso)
+aa$start <- lag(aa$end,default=0)+1
+
---
+++ 03circle.R
@@ -0,0 +1,44 @@
+do.circle <- function(finst) {
+    fsent <- finst %>% group_by(Vieta) %>% summarize(Sent=sum(Viso)) %>% arrange(-Sent) %>% set_names(c("Padalinys","Sent"))
+    frec <- finst %>% group_by(Padalinys) %>% summarize(Received=sum(Viso)) %>% arrange(-Received)
+
+    fsr <- merge(fsent,frec,all=TRUE)
+    fsr$Sent[is.na(fsr$Sent)] <- 0
+    fsr$Received[is.na(fsr$Received)] <- 0
+    fsr <- fsr %>% mutate(Padalinys=as.character(Padalinys)) %>% arrange(Padalinys)
+    
+    fsr <- fsr %>% mutate(All=Sent+Received,col=brewer.pal(10,"Paired"))
+    
+    par(mar = c(1, 1, 1, 1), lwd = 0.1, cex = 0.7)
+    circos.par("default.track.height" = 0.1)
+    circos.initialize(factors = fsr$Padalinys, xlim = cbind(0,fsr$All))
+    
+    circos.trackPlotRegion(factors = fsr$Padalinys, ylim=c(0,1),
+                           panel.fun = function(x, y) {
+                           sector.index = get.cell.meta.data("sector.index")
+                           dd <- fsr %>% filter(Padalinys==sector.index)
+                           circos.rect(0, 0, dd$Sent, 1,
+                                       col = dd$col, border = "black", lty = par("lty"), lwd = par("lwd"))
+                           col2 <- ifelse(sector.index %in% fwin_pdal,"#FFFF99","white")
+                           circos.rect(dd$Sent+1, 0, dd$All, 1,
+                                       col = col2, border = "black", lty = par("lty"), lwd = par("lwd"))
+                           circos.text(x=dd$All/2,y=0.5,label=sector.index)                           
+                       })
+    
+
+    ff <- finst %>% group_by(Padalinys) %>% mutate(Received.end=cumsum(Viso),Received.start=lag(Received.end,default=0))
+    ff <- ff %>% group_by(Vieta) %>% mutate(Sent.end=cumsum(Viso),Sent.start=lag(Sent.end,default=0))
+    
+    places_from <- ff %>% use_series("Padalinys") %>% as.character %>% unique
+
+    for(pfrom in places_from) {
+        places_to <- ff%>% filter(Padalinys==pfrom) %>% use_series(Vieta)
+        for (pto in places_to) {
+            to <- ff%>% filter(Padalinys==pfrom & Vieta==pto)%>%extract(,c("Received.start","Received.end")) %>% as.numeric + fsr%>% filter(Padalinys==pfrom) %>% select(Sent) %>% as.numeric 
+            from <- ff%>% filter(Padalinys==pfrom & Vieta==pto)%>%extract(,c("Sent.start","Sent.end")) %>% as.numeric
+            lcol <- fsr%>% filter(Padalinys==pto) %>% use_series("col")
+            circos.link(pto,from,pfrom,to,col=lcol)
+        }
+    }
+    list(fsr=fsr,ff=ff)
+}
